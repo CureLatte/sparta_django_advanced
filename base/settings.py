de3d4a29dbed39a292b9f123f6e006c8b1f7adad
김재성 <zhqmfkv@naver.com>
2022-02-11 21:01:45 +0900

    startapp tabom and make models

--- base/settings.py
+++ base/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tabom.apps.TabomConfig",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+try:
+    from base.local_settings import *
+except ImportError:
+    pass
