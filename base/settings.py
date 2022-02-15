9e973e7d87a6417e71635842c498918cd80c0ead
김재성 <zhqmfkv@naver.com>
2022-02-15 18:02:57 +0900

    3-4 lession

--- base/settings.py
+++ base/settings.py
@@ -113,11 +113,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -128,7 +128,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 try:
     from base.local_settings import *
---
+++ tabom/services/article_service.py
@@ -0,0 +1,12 @@
+from django.core.paginator import Page, Paginator
+from django.db.models import QuerySet
+
+from tabom.models import Article
+
+
+def get_an_article(article_id: int) -> Article:
+    return Article.objects.filter(id=article_id).get()
+
+
+def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+    return Article.objects.order_by("-id")[offset : offset + limit]
