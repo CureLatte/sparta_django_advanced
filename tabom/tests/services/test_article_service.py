5a4335beeedcc0ca6705f9b6dedecb0e02a33b0b
김재성 <zhqmfkv@naver.com>
2022-02-15 22:44:24 +0900

    e2e test

--- tabom/tests/services/test_article_service.py
+++ tabom/tests/services/test_article_service.py
@@ -2,11 +2,26 @@ from django.test import TestCase
 
 from tabom.models import Like, User
 from tabom.models.article import Article
-from tabom.services.article_service import get_an_article, get_article_list
+from tabom.services.article_service import (
+    create_an_article,
+    delete_an_article,
+    get_an_article,
+    get_article_list,
+)
 from tabom.services.like_service import do_like
 
 
 class TestArticleService(TestCase):
+    def test_you_can_create_an_article(self) -> None:
+        # Given
+        title = "test_title"
+
+        # When
+        article = create_an_article(title)
+
+        # Then
+        self.assertEqual(article.title, title)
+
     def test_you_can_get_an_article_by_id(self) -> None:
         # Given
         title = "test_title"
@@ -62,7 +77,7 @@ class TestArticleService(TestCase):
 
     def test_get_article_list_should_not_contain_my_likes_when_user_id_is_zero(self) -> None:
         # Given
-        user = User.objects.create(name="test_user")
+        user = User.objects.create(name="test_user")  #
         article1 = Article.objects.create(title="artice1")
         Like.objects.create(user_id=user.id, article_id=article1.id)
         Article.objects.create(title="article2")
@@ -74,3 +89,16 @@ class TestArticleService(TestCase):
         # Then
         self.assertEqual(0, len(articles[1].my_likes))
         self.assertEqual(0, len(articles[0].my_likes))
+
+    def test_you_can_delete_an_article(self) -> None:
+        # Given
+        user = User.objects.create(name="user1")
+        article = Article.objects.create(title="artice1")
+        like = do_like(user.id, article.id)
+
+        # When
+        delete_an_article(article.id)
+
+        # Then
+        self.assertFalse(Article.objects.filter(id=article.id).exists())
+        self.assertFalse(Like.objects.filter(id=like.id).exists())
