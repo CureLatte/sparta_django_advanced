9f2816cdac3e8c0a93c7f1c45522c33193def4f4
김재성 <zhqmfkv@naver.com>
2022-02-17 17:33:54 +0900

    async add!

--- tabom/apis/v1/like_router.py
+++ tabom/apis/v1/like_router.py
@@ -8,34 +8,25 @@ from ninja.errors import HttpError
 from tabom.apis.v1.schemas.like_request import LikeRequest
 from tabom.apis.v1.schemas.like_response import LikeResponse
 from tabom.models import Article, Like, User
-from tabom.services.like_service import do_like, undo_like
+from tabom.services.like_service import async_do_like, async_undo_like
 
 router = Router(tags=["likes"])
 
 
 @router.post("/", response={201: LikeResponse})
-def post_like(request: HttpRequest, like_request: LikeRequest) -> Tuple[int, Like]:
-
+async def post_like(request: HttpRequest, like_request: LikeRequest) -> Tuple[int, Like]:
     try:
-        # 좋아요 갯수 추가
-        like = do_like(like_request.user_id, like_request.article_id)
-
+        like = await async_do_like(user_id=like_request.user_id, article_id=like_request.article_id)
     except User.DoesNotExist:
-        # 유저가 존재하지 않을 경우
         raise HttpError(404, f"User #{like_request.user_id} Not Found")
-
     except Article.DoesNotExist:
-        # 기사가 존재하지 않을 경우
-        raise HttpError(404, f"Article id #{like_request.article_id} Not Found")
-
+        raise HttpError(404, f"Article #{like_request.article_id} Not Found")
     except IntegrityError:
-        # 두번 누를 경우
         raise HttpError(400, "duplicate like")
-
     return 201, like
 
 
 @router.delete("/", response={204: None})
-def delete_like(request: HttpRequest, user_id: int, article_id: int) -> Tuple[int, None]:
-    undo_like(user_id, article_id)
+async def delete_like(request: HttpRequest, user_id: int, article_id: int) -> Tuple[int, None]:
+    await async_undo_like(user_id=user_id, article_id=article_id)
     return 204, None
